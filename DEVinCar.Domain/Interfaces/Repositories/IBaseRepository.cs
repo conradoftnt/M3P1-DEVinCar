9694374b1cafec8c189ac14f46a89981acfe1704
conrado <conradofortunato246@gmail.com>
2022-10-08 13:43:55 -0300

    feat: adiciona interface para repositorio base

---
+++ DEVinCar.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+namespace DEVinCar.Domain.Interfaces.Repositories;
+
+public interface IBaseRepository<TEntity, TKey>
+{
+    TEntity GetById(TKey id);
+    List<TEntity> GetAll();
+    void Insert(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TKey id);
+
+}
