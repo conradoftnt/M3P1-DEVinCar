a1abd53a8ff8d6a7335df21ecf954539b0802c08
conrado <conradofortunato246@gmail.com>
2022-10-09 14:41:59 -0300

    fix: atualizando atributo como required

--- DEVinCar.Domain/DTOs/DeliveryDTO.cs
+++ DEVinCar.Domain/DTOs/DeliveryDTO.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DEVinCar.Domain.DTOs;
 
 public class DeliveryDTO
 {
-    public int? AddressId { get; set; }
+    [Required(ErrorMessage = "The address id is required")]
+    public int AddressId { get; set; }
+    
     public DateTime? DeliveryForecast { get; set; }
 }
