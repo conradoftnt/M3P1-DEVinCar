6a3c91617b625ffeb12f7bae3883921583938489
conrado <conradofortunato246@gmail.com>
2022-10-09 14:59:35 -0300

    feat: services configuradas na program

--- DEVinCar.Api/Program.cs
+++ DEVinCar.Api/Program.cs
@@ -2,17 +2,25 @@ using System.Text.Json.Serialization;
 using DEVinCar.Infra.Data;
 using DEVinCar.Domain.Interfaces.Repositories;
 using DEVinCar.Infra.Data.Repositories;
+using DEVinCar.Domain.Interfaces.Services;
+using DEVinCar.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DevInCarDbContext>();
 
 builder.Services.AddScoped<IAddressesRepository,AddressesRepository>();
+builder.Services.AddScoped<IAddressesService,AddressesService>();
 builder.Services.AddScoped<ICarRepository,CarRepository>();
+builder.Services.AddScoped<ICarsService,CarsService>();
 builder.Services.AddScoped<IDeliverRepository,DeliverRepository>();
+builder.Services.AddScoped<IDeliverService,DeliverService>();
 builder.Services.AddScoped<ISalesRepository,SalesRepository>();
+builder.Services.AddScoped<ISalesService,SalesService>();
 builder.Services.AddScoped<IStatesRepository,StatesRepository>();
+builder.Services.AddScoped<IStatesService,StatesService>();
 builder.Services.AddScoped<IUsersRepository,UsersRepository>();
+builder.Services.AddScoped<IUsersService,UsersService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
